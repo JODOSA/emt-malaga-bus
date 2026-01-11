966d1075db97fb8241f18c45b7f7c924004fbe63
Joaquín <32976960+JODOSA@users.noreply.github.com>
2026-01-11 08:24:49 +0100

    Crear servidor Express con rutas básicas de API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,34 @@
-console.log("TypeScript funcionando");
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Ruta de prueba
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'API de EMT Málaga funcionando'});
+});
+
+// Ruta de prueba para paradas
+app.get('/api/paradas', (req:Request, res:Response) => {
+    res.json({
+        message: 'Aquí irán las paradas cercanas',
+        paradas:[]
+    });
+});
+
+// Ruta de preba para horarios
+app.get('/api/horarios/:paradaId', (req:Request, res:Response) => {
+    const { paradaId } = req.params;
+    res.json({
+        message: `Aquí irán los horarios de la parada ${paradaId}`,
+        horarios:[]
+    });
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
